bbf4c2bb811905817714ee5e860f1740c9732d52
ADITYA SINGH <59895604+imvas@users.noreply.github.com>
2020-05-09 16:27:33 +0530

    Create kaprekar.py
    set() is an inbuilt function which convert digits into particular string

---
+++ kaprekar.py
@@ -0,0 +1,16 @@
+def kaprekarNumbers(p, q):
+    k=0
+    if p<9:
+        p=9
+        print(1,end=" ")
+    for i in range(p,q+1):
+        n=i**2
+        n=str(n)
+        d=len(n)//2
+        num1=n[:d]
+        num2=n[d:]
+        if i==(int(num1)+int(num2)):
+            print(i,end=" ")
+            k=-1
+    if k==0:
+        print("INVALID RANGE")
